081be448f4367793613c9968b1263eff10abd9d6
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2020-03-31 00:22:01 -0300

    Add initial files

---
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/Light.java
@@ -0,0 +1,7 @@
+package br.pro.hashi.ensino.desagil.aps.model;
+
+public class Light implements Receiver {
+    @Override
+    public void connect(int inputIndex, Emitter emitter) {
+    }
+}
---
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/Switch.java
@@ -0,0 +1,8 @@
+package br.pro.hashi.ensino.desagil.aps.model;
+
+public class Switch implements Emitter {
+    @Override
+    public boolean read() {
+        return false;
+    }
+}
