b1149bf0e1d8f407c86a2f545228bbc5f58b8d75
vitorbandeira1 <vitorirulegui@gmail.com>
2020-04-17 15:36:50 -0300

    Merge certo

--- src/main/java/br/pro/hashi/ensino/desagil/aps/model/AndGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/AndGate.java
@@ -1,12 +1,25 @@
 package br.pro.hashi.ensino.desagil.aps.model;
 
 public class AndGate extends Gate {
+<<<<<<< HEAD
+
+    private final NandGate nand1, nand2;
+=======
     private final NandGate nandLeft;
     private final NandGate nandRight;
+>>>>>>> 99e9111408edd710ad810ac9abd8fee66cb72796
 
     public AndGate() {
         super("AND", 2);
 
+<<<<<<< HEAD
+        nand1 = new NandGate();
+        nand2 = new NandGate();
+    }
+
+    public boolean read() {
+        return nand2.read();
+=======
         nandLeft = new NandGate();
 
         nandRight = new NandGate();
@@ -17,6 +30,7 @@ public class AndGate extends Gate {
     @Override
     public boolean read() {
         return nandRight.read();
+>>>>>>> 99e9111408edd710ad810ac9abd8fee66cb72796
     }
 
     @Override
@@ -24,6 +38,23 @@ public class AndGate extends Gate {
         if (inputIndex < 0 || inputIndex > 1) {
             throw new IndexOutOfBoundsException(inputIndex);
         }
+<<<<<<< HEAD
+
+        if (inputIndex == 0) {
+            nand1.connect(0, emitter);
+        }
+        if (inputIndex == 1) {
+            nand1.connect(1, emitter);
+        }
+
+        nand2.connect(0, nand1);
+        nand2.connect(1, nand1);
+
+    }
+}
+
+=======
         nandLeft.connect(inputIndex, emitter);
     }
 }
+>>>>>>> 99e9111408edd710ad810ac9abd8fee66cb72796
